cf242776f3eed001b19ad59e4614190b79528797
Azra Park <azrajamesp@gmail.com>
2023-11-03 15:35:45 -0500

    Update CYOA.py

--- CYOA.py
+++ CYOA.py
@@ -15,7 +15,7 @@ with open("choose_your_own_adventure-token.json") as f:
 CLIENT_ID = secrets["clientId"]
 CLIENT_SECRET = secrets["secret"]
 ASTRA_DB_KEYSPACE = "database"
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = secrets["openAi"]
 
 auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
